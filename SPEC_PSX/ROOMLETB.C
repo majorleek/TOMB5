9a36ea8d69b573182cf902664c8c903ea471e1d9
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-03-16 11:31:31 +0000

    Backup

--- SPEC_PSX/ROOMLETB.C
+++ SPEC_PSX/ROOMLETB.C
@@ -1,9 +1,21 @@
 #include "ROOMLETB.H"
 
+#include "CAMERA.H"
+#include "DRAW.H"
 #include "SPECIFIC.H"
 #include "SPECTYPES.H"
 
 void DrawRoomletListAsmBinocular(long underwater, struct room_info* r)//roomletb
 {
 	S_Warn("[DrawRoomletListAsmBinocular] - Unimplemented!\n");
+}
+
+void sub_1BC4D0(struct room_info* r)//roomletb
+{
+
+}
+
+void DrawRoomletList()
+{
+	sub_1BC4D0(&room[camera.pos.room_number]);
 }
